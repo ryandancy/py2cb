2a47cdd509bcf5696decfbaff0a2b595359af2fe
Copper <git@keal.ca>
2016-03-14 18:32:52 -0400

    Wrapped CLI arguments getting in method

--- py2cb/__init__.py
+++ py2cb/__init__.py
@@ -5,10 +5,14 @@ import argparse
 
 __author__ = 'Copper'
 
-parser = argparse.ArgumentParser(description='Compiles Python to Minecraft command blocks')
-parser.add_argument('input_file', dest='infile', type=str, help='The Python file to be read.')
-parser.add_argument(['--output-file', '--output', '-o'], dest='dumpfile', type=str,
-                    help='The file to which commands will be dumped. Incompatible with --schematic-file/--schematic/-s')
-parser.add_argument(['--schematic-file', '--schematic', '-s'], dest='schemfile', type=str,
-                    help='The file to which the schematic will be dumped. Incompatible with --output-file/--output/-o')
-parsed_args = parser.parse_args()
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='Compiles Python to Minecraft command blocks')
+    parser.add_argument('input_file', dest='infile', type=str, help='The Python file to be read.')
+    parser.add_argument(
+        ['--output-file', '--output', '-o'], dest='dumpfile', type=str,
+        help='The file to which commands will be dumped. Incompatible with --schematic-file/--schematic/-s')
+    parser.add_argument(
+        ['--schematic-file', '--schematic', '-s'], dest='schemfile', type=str,
+        help='The file to which the schematic will be dumped. Incompatible with --output-file/--output/-o')
+    return parser.parse_args()

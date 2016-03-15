9d3802cab54ab807382bb76bc223c78c234f0d68
Copper <git@keal.ca>
2016-03-15 06:56:36 -0400

    Added the CommandBlock class.

--- py2cb/__init__.py
+++ py2cb/__init__.py
@@ -7,6 +7,40 @@ import ast
 __author__ = 'Copper'
 
 
+class CommandBlock:
+
+    IMPULSE = 0
+    CHAIN = 1
+    REPEAT = 2
+
+    DOWN = 0
+    UP = 1
+    NORTH = 2
+    SOUTH = 3
+    WEST = 4
+    EAST = 5
+
+    CONDITIONAL = 8
+
+    def __init__(self, command, type_=IMPULSE, metadata=EAST, auto=True):
+        self.command = command
+        self.type_ = type_
+        self.metadata = metadata
+        self.auto = auto
+
+    def _get_command_block_name(self):
+        return {
+            CommandBlock.IMPULSE: 'command_block',
+            CommandBlock.CHAIN: 'chain_command_block',
+            CommandBlock.REPEAT: 'repeating_command_block'
+        }[self.type_]
+
+    def get_gen_command(self, offx, offy, offz):
+        return 'setblock ~{0} ~{1} ~{2} minecraft:{3} {4} replace {{"Command":"{5}","auto":{6}b}}'.format(
+            offx, offy, offz, self._get_command_block_name(), self.metadata, self.command, int(self.auto)
+        )
+
+
 def get_ast(code, filename):
     return ast.parse(code, filename=filename)
 

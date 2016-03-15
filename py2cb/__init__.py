36b0f0962724802bb2f18342b25230d30fa232ad
Copper <git@keal.ca>
2016-03-15 06:19:31 -0400

    Made -o and -s mutually exclusive

--- py2cb/__init__.py
+++ py2cb/__init__.py
@@ -9,10 +9,12 @@ __author__ = 'Copper'
 def parse_args():
     parser = argparse.ArgumentParser(description='Compiles Python to Minecraft command blocks')
     parser.add_argument('input_file', dest='infile', type=str, help='The Python file to be read.')
-    parser.add_argument(
+
+    output_group = parser.add_mutually_exclusive_group(required=True)
+    output_group.add_argument(
         ['--output-file', '--output', '-o'], dest='dumpfile', type=str,
         help='The file to which commands will be dumped. Incompatible with --schematic-file/--schematic/-s')
-    parser.add_argument(
+    output_group.add_argument(
         ['--schematic-file', '--schematic', '-s'], dest='schemfile', type=str,
         help='The file to which the schematic will be dumped. Incompatible with --output-file/--output/-o')
     return parser.parse_args()

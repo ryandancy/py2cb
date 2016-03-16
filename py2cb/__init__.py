7c08bbe5eb5a57385ac7ff1586c42f5ab650ca8e
Copper <git@keal.ca>
2016-03-16 11:20:16 -0400

    Changed / operation's Python equivalent to FloorDiv from Div for better compatibility.

--- py2cb/__init__.py
+++ py2cb/__init__.py
@@ -175,11 +175,11 @@ def get_op_char(binop: ast.BinOp) -> str:
             ast.Add: '+',
             ast.Sub: '-',
             ast.Mult: '*',
-            ast.Div: '/',
+            ast.FloorDiv: '/',
             ast.Mod: '%'
         }[type(binop.op)]
     except KeyError:
-        raise Exception('Invalid operation (only +, -, *, /, % are allowed).')
+        raise Exception('Invalid operation (only +, -, *, //, % are allowed).')
 
 
 def setup_internal_values(node: ast.AST, contr: Contraption, x: int, y: int, z: int) \

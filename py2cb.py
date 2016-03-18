cd12f29ca12d9eedfb3b9e36904ecf44b73f65ad
Copper <git@keal.ca>
2016-03-18 15:17:26 -0400

    Added say and tellraw functions, with formatting codes for tellraw

--- py2cb.py
+++ py2cb.py
@@ -3,11 +3,87 @@
 
 import argparse
 import ast
+import colorama
 
 from pynbt import NBTFile, TAG_Byte, TAG_Byte_Array, TAG_Compound, TAG_Int, TAG_List, TAG_Short, TAG_String
 from typing import Tuple, List, Any, Optional
+from colorama import Fore, Style
 
 __author__ = 'Copper'
+
+colorama.init(autoreset=True)
+
+
+# ======= FUNCTIONS FOR SCRIPT USE ======= #
+# This allows a valid script to be run in Python and behave the same (ish) as in Minecraft
+
+
+def say(*args):
+    print('[@] ', *args, sep='')
+
+
+# Flags for tellraw
+BLACK = 0x000
+DARK_BLUE = 0x001
+DARK_GREEN = 0x002
+DARK_AQUA = 0x003
+DARK_RED = 0x004
+DARK_PURPLE = 0x005
+GOLD = DARK_YELLOW = 0x006
+GRAY = GREY = 0x007
+DARK_GRAY = DARK_GREY = 0x008
+BLUE = 0x009
+GREEN = 0x00A
+AQUA = 0x00B
+RED = 0x00C
+LIGHT_PURPLE = PURPLE = 0x00D
+YELLOW = 0x00E
+WHITE = 0x00F
+BOLD = 0x010
+ITALICS = 0x020
+UNDERLINED = 0x040
+STRIKETHROUGH = 0x080
+OBFUSCATED = 0x100
+
+# For colours with DARK_ variants, the DARK_ version is ANSI's 'normal', and the light/normal version is ANSI's 'bright'
+mc_to_ansi = {
+    BLACK: Fore.BLACK,
+    DARK_BLUE: Fore.BLUE,
+    DARK_GREEN: Fore.GREEN,
+    DARK_AQUA: Fore.CYAN,
+    DARK_RED: Fore.RED,
+    DARK_PURPLE: Fore.MAGENTA,
+    DARK_YELLOW: Fore.YELLOW,  # also GOLD
+    GREY: Fore.WHITE + Style.DIM,  # also GRAY
+    DARK_GREY: Fore.BLACK + Style.BRIGHT,  # also DARK_GRAY
+    BLUE: Fore.BLUE + Style.BRIGHT,
+    GREEN: Fore.GREEN + Style.BRIGHT,
+    AQUA: Fore.CYAN + Style.BRIGHT,
+    RED: Fore.RED + Style.BRIGHT,
+    PURPLE: Fore.MAGENTA + Style.BRIGHT,  # also LIGHT_PURPLE
+    YELLOW: Fore.YELLOW + Style.BRIGHT,
+    WHITE: Fore.WHITE + Style.BRIGHT,
+    # None of the non-color formatting flags are *really* supported
+    BOLD: '[BOLD] ',
+    ITALICS: '[ITALICS] ',
+    UNDERLINED: '[UNDERLINED] ',  # There is an ANSI escape for this, but there's no equivalent on Windows
+    STRIKETHROUGH: '[STRIKETHROUGH] ',  # Same
+    OBFUSCATED: '[OBFUSCATED] '
+}
+
+
+def tellraw(*args):
+    """
+    Each formatted arg is a tuple (*stuff, flags). Uses ANSI colour codes + Colorama.
+    """
+    for arg in args:
+        if isinstance(arg, tuple):
+            print(mc_to_ansi[arg[-1]], *arg[:-1], sep='', end='')
+        else:
+            print(arg, end='')
+
+
+# ======= COMPILER ======= #
 
 
 class CommandBlock:

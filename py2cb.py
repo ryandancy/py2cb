8772361c6b15a7632258b3abbf46fb9127cdff36
Copper <git@keal.ca>
2016-03-20 10:44:14 -0400

    Fixed a bug where target.id was being used instead of target

--- py2cb.py
+++ py2cb.py
@@ -527,7 +527,7 @@ def parse_node(node: ast.AST, contr: Contraption, x: int, y: int, z: int) -> Tup
                         x += 1
                         contr.add_block((x, y, z), CommandBlock(
                             'scoreboard players set @e[type=ArmorStand,tag=list_noname] py2cb_ids {0}'
-                                .format(listids[target.id])
+                                .format(listids[target])
                         ))
                         x += 1
                         contr.add_block((x, y, z), CommandBlock(

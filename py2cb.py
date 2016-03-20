ff82a49618537ea8958ac8249ba868f55c1bc39d
Copper <git@keal.ca>
2016-03-20 15:10:37 -0400

    Fixed duplicate braces around tellraw args.

--- py2cb.py
+++ py2cb.py
@@ -1062,7 +1062,7 @@ def parse_node(node: ast.AST, contr: Contraption, x: int, y: int, z: int) -> Tup
             
             x += 1
             contr.add_block((x, y, z), CommandBlock(
-                'tellraw {0} [{1}]'.format(to, ','.join('{' + json_arg + '}' for json_arg in json_args))
+                'tellraw {0} [{1}]'.format(to, ','.join(json_arg for json_arg in json_args))
             ))
         
         # something else
